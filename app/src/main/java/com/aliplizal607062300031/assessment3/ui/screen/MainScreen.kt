ded544ddd783ad97c6217f46cb59fb3250614d5e
aliplizal <aliplizal11@gmail.com>
2025-06-13 01:05:17 +0700

    Membuat tampilan nama hewan

--- app/src/main/java/com/aliplizal607062300031/assessment3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aliplizal607062300031/assessment3/ui/screen/MainScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -23,8 +25,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -81,6 +86,29 @@ fun ListItem(buku: Buku) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
+        Column(
+                modifier = Modifier.fillMaxWidth().padding(4.dp)
+            .background(Color(red = 0f, green = 0f, blue = 0f, alpha = 0.5f))
+                .padding(4.dp)
+        ) {
+            Text(
+                text = buku.judul,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Text(
+                text = buku.kategori,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+            Text(
+                text = buku.status,
+                fontStyle = FontStyle.Italic,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
     }
 }
 
